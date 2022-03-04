2e6c2850f893134cabde3880b54749c1620344af
Victorywastaken <rlatmdfl92@gmail.com>
2022-03-04 18:28:06 -0500

    Bitwise operators

--- blackJack.js
+++ blackJack.js
@@ -1,7 +1,20 @@
-import Deck from './deckOfCards.js';
+// import Deck from './deckOfCards.js';
 
-let deck = new Deck();
-console.log(deck);
-// class Blackjack {
+// let deck = new Deck();
+// console.log(deck);
 
+// class BlackJack {
+//   constructor(){
+//     this.playerHand = [];
+//     this.dealerHand = [];
+//     this.playerScore = 0;
+//     this.dealerScore = 0;
+//   }
 // }
+
+function add(num1, num2){
+  //add two numbers without using + or - using bitwise operators
+  return (num1 ^ num2) & (num1 | num2);
+}
+
+console.log(add(5, 8));
